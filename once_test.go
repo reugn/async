22d2d3bf895521ae1c4262ce32acde30e87a39e6
Eugene R <reugpro@gmail.com>
2023-08-11 10:55:43 +0300

    feat: add async.Once implementation (#11)

---
+++ once_test.go
@@ -0,0 +1,57 @@
+package async
+
+import (
+	"sync"
+	"sync/atomic"
+	"testing"
+
+	"github.com/reugn/async/internal"
+)
+
+func TestOnce(t *testing.T) {
+	var once Once[int32]
+	var count int32
+
+	for i := 0; i < 10; i++ {
+		count, _ = once.Do(func() (int32, error) {
+			count++
+			return count, nil
+		})
+	}
+	internal.AssertEqual(t, count, 1)
+}
+
+func TestOnceConcurrent(t *testing.T) {
+	var once Once[int32]
+	var count int32
+	var wg sync.WaitGroup
+
+	for i := 0; i < 10; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			result, _ := once.Do(func() (int32, error) {
+				newCount := atomic.AddInt32(&count, 1)
+				return newCount, nil
+			})
+			atomic.StoreInt32(&count, result)
+		}()
+	}
+	wg.Wait()
+	internal.AssertEqual(t, count, 1)
+}
+
+func TestOncePanic(t *testing.T) {
+	var once Once[int32]
+	var count int32
+	var err error
+
+	for i := 0; i < 10; i++ {
+		count, err = once.Do(func() (int32, error) {
+			count /= count
+			return count, nil
+		})
+	}
+	internal.AssertEqual(t, err.Error(), "recovered runtime error: integer divide by zero")
+	internal.AssertEqual(t, count, 0)
+}
