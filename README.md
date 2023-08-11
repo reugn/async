6410dc9101a1c3367a79560e17e87010a3a51b43
reugn <reugpro@gmail.com>
2023-08-11 10:52:22 +0300

    feat: add async.Once implementation

--- README.md
+++ README.md
@@ -12,6 +12,7 @@ Async is a synchronization and asynchronous computation package for Go.
 * **Future** - A placeholder object for a value that may not yet exist.
 * **Promise** - While futures are defined as a type of read-only placeholder object created for a result which doesn’t yet exist, a promise can be thought of as a writable, single-assignment container, which completes a future.
 * **Task** - A data type for controlling possibly lazy and asynchronous computations.
+* **Once** - An object similar to sync.Once having the Do method taking `f func() (T, error)` and returning `(T, error)`.
 * **WaitGroupContext** - A WaitGroup with the `context.Context` support for graceful unblocking.
 * **Reentrant Lock** - Mutex that allows goroutines to enter into the lock on a resource more than once.
 * **Optimistic Lock** - Mutex that allows optimistic reading. Could be retried or switched to RLock in case of failure. Significantly improves performance in case of frequent reads and short writes. See [benchmarks](./benchmarks/README.md).
---
+++ once.go
@@ -0,0 +1,40 @@
+package async
+
+import (
+	"fmt"
+	"sync"
+)
+
+// Once is an object that will execute the given function exactly once.
+// Any subsequent call will return the previous result.
+type Once[T any] struct {
+	runOnce sync.Once
+	result  T
+	err     error
+}
+
+// Do calls the function f if and only if Do is being called for the
+// first time for this instance of Once. In other words, given
+//
+//	var once Once
+//
+// if once.Do(f) is called multiple times, only the first call will invoke f,
+// even if f has a different value in each invocation. A new instance of
+// Once is required for each function to execute.
+//
+// The return values for each subsequent call will be the result of the
+// first execution.
+//
+// If f panics, Do considers it to have returned; future calls of Do return
+// without calling f
+func (o *Once[T]) Do(f func() (T, error)) (T, error) {
+	o.runOnce.Do(func() {
+		defer func() {
+			if err := recover(); err != nil {
+				o.err = fmt.Errorf("recovered %v", err)
+			}
+		}()
+		o.result, o.err = f()
+	})
+	return o.result, o.err
+}
